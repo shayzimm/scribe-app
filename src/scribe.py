f6eb45cf4b0666bfe2498b064c301189927ba333
shayzimm <shay.zimmerle@gmail.com>
2024-05-03 19:11:48 +0800

    more docstrings added

--- src/scribe.py
+++ src/scribe.py
@@ -20,7 +20,7 @@ class Journal:
        self.id = last_id  
 
    def match(self, filter):
-         """
+        """
         Checks if the journal matches the filter text.
         Return True if it matches exactly, False if it does not match.
         Filter is case-sensitive.
@@ -41,13 +41,39 @@ class Journal:
 class JournalBook:   
     '''Represent a collection of journals'''
     def __init__(self):
-        ''' Initialise journalbook with an empty list'''
+        '''
+        Initialise journalbook with an empty list.
+
+        This method initializes a new instance of JournalBook, which is a collection of Journal entries.
+        It creates an empty list to store the journals.
+
+        :return: None
+        :rtype: None
+        '''
         self.journals = []
 
     def new_journal(self, memo, tags=''):
-       ''' Creates a new journal entry in the journalbook '''
+       """
+        Creates a new journal entry in the journalbook.
+
+        :param memo: The content of the journal entry.
+        :type memo: str
+        :param tags: Optional tags for the journal entry. Default is an empty string.
+        :type tags: str, optional
+
+        :return: None
+        :rtype: None
+        """
        self.journals.append(Journal(memo, tags))
 
     def search_journal(self, filter):
-      ''' searches all journal entries that match the filter '''
-      return [ journal for journal in self.journals if journal.match(filter)]
+      """
+        Searches all journal entries that match the filter.
+
+        :param filter: The text to search for in the journal's memo and tags.
+        :type filter: str
+
+        :return: A list of Journal objects that match the filter.
+        :rtype: list
+        """
+      return [journal for journal in self.journals if journal.match(filter)]
