614c7abae6818b0a97b30091a1b345f92ef37e6c
shayzimm <shay.zimmerle@gmail.com>
2024-05-03 19:31:15 +0800

    syntax/formatting

--- src/main.py
+++ src/main.py
@@ -2,10 +2,10 @@ import sys
 from scribe import Journal, JournalBook
 
 class Menu:
- """
-    Initialize the Menu class.
+    """
+    Initialise the Menu class.
 
-    Initialize the JournalBook object and define the choices dictionary.
+    Initialise the JournalBook object and define the choices dictionary.
 
     Args:
     None
@@ -13,17 +13,17 @@ class Menu:
     Returns:
     None
     """
- def __init__(self):
-      self.journalbook = JournalBook()
-      self.choices = {
-           "1" : self.show_journals,
-           "2" : self.add_journal,
-           "3" : self.search_journals,
-           "4" : self.quit
+    def __init__(self):
+        self.journalbook = JournalBook()
+        self.choices = {
+            "1" : self.show_journals,
+            "2" : self.add_journal,
+            "3" : self.search_journals,
+            "4" : self.quit
         }
 
- def display_menu(self):
-       """
+    def display_menu(self):
+        """
         Display Scribe's menu.
 
         Prints a welcome message and a list of available options for the user.
@@ -34,17 +34,17 @@ class Menu:
         Returns:
         None
         """
-       print(""" 
-             Welcome to Scribe
-             What would you like to do today?
-             1. Show journal entries
-             2. Add journal
-             3. Search journals
-             4. Quit program
-             """)
+        print("""
+              Welcome to Scribe
+              What would you like to do today?
+              1. Show a list of all journal entries
+              2. Add a journal entry
+              3. Search journals
+              4. Quit program
+              """)
 
- def run(self):
-     """
+    def run(self):
+        """
         Display menu and respond to user choices.
 
         This method continuously displays the menu and responds to user input by
@@ -55,88 +55,88 @@ class Menu:
 
         Returns:
         None
-    """
-     while True:
-           self.display_menu()
-           choice = input("Enter an option: " )
-           action = self.choices.get(choice)
-           if action:
+        """
+        while True:
+            self.display_menu()
+            choice = input("Enter an option: " )
+            action = self.choices.get(choice)
+            if action:
                 action()
-           else:
-              print("{0} is not a valid choice".format(choice))
+            else:
+                print("{0} is not a valid choice".format(choice))
 
- def show_journals(self, journals=None):
-     """
-    Display all journal entries in journalbook.
+    def show_journals(self, journals=None):
+        """
+        Display all journal entries in journalbook.
 
-    If no journals are provided, it will use the journals from the journalbook object.
+        If no journals are provided, it will use the journals from the journalbook object.
 
-    Args:
-    journals (list, optional): A list of Journal objects. Defaults to None.
+        Args:
+        journals (list, optional): A list of Journal objects. Defaults to None.
 
-    Returns:
-    None: This function does not return any value. It prints the journal entries.
-    """
-     if not journals:
-        journals = self.journalbook.journals
-     for journal in journals:
-       print("{0}".format(journal.memo))
+        Returns:
+        None: This function does not return any value. It prints the journal entries.
+        """
+        if not journals:
+            journals = self.journalbook.journals
+        for journal in journals:
+            print("{0}".format(journal.memo))
 
- def add_journal(self):
-     """
-    Add a new journal entry to the JournalBook.
+    def add_journal(self):
+        """
+        Add a new journal entry to the JournalBook.
 
-    Args:
-    None
+        Args:
+        None
 
-    Returns:
-    None: This function does not return any value. It adds the journal entry to the JournalBook.
-    """
-     memo = input("Your entry: " )
-     self.journalbook.new_journal(memo)
-     print("Your entry has been added")   
+        Returns:
+        None: This function does not return any value. It adds the journal entry to the JournalBook.
+        """
+        memo = input("Your entry: " )
+        self.journalbook.new_journal(memo)
+        print("Your entry has been added")
 
- def search_journals(self):
-     """
-    Search for a specific journal in the journalbook using the match filter.
+    def search_journals(self):
+        """
+        Search for a specific journal in the journalbook using the match filter.
 
-    Args:
-    None
+        Args:
+        None
 
-    Returns:
-    None: This function does not return any value. It prints the matching journal entries.
+        Returns:
+        None: This function does not return any value. It prints the matching journal entries.
 
-    Raises:
-    ValueError: If no matching journals are found.
-    """
-     filter = input("Search for:  ")
-     journals = self.journalbook.search_journal(filter)
-     self.show_journals(journals)
+        Raises:
+        ValueError: If no matching journals are found.
+        """
+        filter = input("Search for:  ")
+        journals = self.journalbook.search_journal(filter)
+        self.show_journals(journals)
 
- def quit(self):
-      """
-    Terminate the program.
+    def quit(self):
+        """
+        Terminate the program.
 
-    This method prints a thank you message and exits the program.
+        This method prints a thank you message and exits the program.
 
-    Args:
-    None
+        Args:
+        None
 
-    Returns:
-    None: This function does not return any value. It terminates the program.
-    """
-      print("Thank you for using Scribe today")
-      sys.exit(0)
+        Returns:
+        None: This function does not return any value. It terminates the program.
+        """
+        print("Thank you for using Scribe today")
+        sys.exit(0)
 
 if __name__ == "__main__":
     """
     This is the main entry point of the program.
-    It initializes the Menu class and runs the application.
+    It initialises the Menu class and runs the application.
 
     Args:
     None
 
     Returns:
-    None: This function does not return any value. It initializes the Menu class and runs the application.
+    None: This function does not return any value. It initialises the Menu class and runs the application.
     """
     Menu().run()
--- src/scribe.py
+++ src/scribe.py
@@ -4,22 +4,22 @@ import datetime
 last_id = 0
 
 class Journal:
-   def __init__(self, memo, tags=' '):
-       """
+    def __init__(self, memo, tags=' '):
+        """
         Initialises a new journal entry with memo and tags. Creation date of new journal and id are automatically set.
         :param memo: The content of the journal entry.
         :type memo: str
         :param tags: Optional tags for the journal entry. Default is an empty string.
         :type tags: str, optional
         """
-       self.memo = memo
-       self.tags = tags
-       self.creation_date = datetime.date.today()
-       global last_id
-       last_id +=1
-       self.id = last_id  
+        self.memo = memo
+        self.tags = tags
+        self.creation_date = datetime.date.today()
+        global last_id
+        last_id +=1
+        self.id = last_id  
 
-   def match(self, filter):
+    def match(self, filter):
         """
         Checks if the journal matches the filter text.
         Return True if it matches exactly, False if it does not match.
@@ -44,7 +44,7 @@ class JournalBook:
         '''
         Initialise journalbook with an empty list.
 
-        This method initializes a new instance of JournalBook, which is a collection of Journal entries.
+        This method initialises a new instance of JournalBook, which is a collection of Journal entries.
         It creates an empty list to store the journals.
 
         :return: None
@@ -53,7 +53,7 @@ class JournalBook:
         self.journals = []
 
     def new_journal(self, memo, tags=''):
-       """
+        """
         Creates a new journal entry in the journalbook.
 
         :param memo: The content of the journal entry.
@@ -64,10 +64,10 @@ class JournalBook:
         :return: None
         :rtype: None
         """
-       self.journals.append(Journal(memo, tags))
+        self.journals.append(Journal(memo, tags))
 
     def search_journal(self, filter):
-      """
+        """
         Searches all journal entries that match the filter.
 
         :param filter: The text to search for in the journal's memo and tags.
@@ -76,4 +76,4 @@ class JournalBook:
         :return: A list of Journal objects that match the filter.
         :rtype: list
         """
-      return [journal for journal in self.journals if journal.match(filter)]
+        return [journal for journal in self.journals if journal.match(filter)]
